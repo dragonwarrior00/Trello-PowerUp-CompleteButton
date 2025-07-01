72d5cf528c6b928c15ca75ab780cb5de99fb963d
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-01 14:45:01 -0500

    Add card detail badge button to power-up.

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,7 +2,21 @@ console.log("Hello World!")
 
 window.TrelloPowerUp.initialize({
   "card-detail-badges": function (t, opts) {
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
 
-        return [];
+        return [
+          {
+            // its best to use static badges unless you need your badges
+            // to refresh you can mix and match between static and dynamic
+            title: "Complete Button",
+            text: "Complete",
+            color: Green,
+          },
+        ];
+      });
   },
 });
