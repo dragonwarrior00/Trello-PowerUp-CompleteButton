3c5e1d5045031b9dec7a70ef71ac3571c663a11b
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-01 14:14:22 -0500

    Correct issues with files

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,11 +2,6 @@ console.log("Hello World!")
 
 window.TrelloPowerUp.initialize({
   "card-detail-badges": function (t, opts) {
-    return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
-        console.log("We just loaded the card name for fun: " + cardName);
 
         return [];
   },
