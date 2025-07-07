0c21c5688e6fd123f4d6d36c8c2b23bb161f94ec
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-07 11:37:36 -0500

    Update connector.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -11,7 +11,7 @@ window.TrelloPowerUp.initialize({
         return [
           {
             // create detail badge itself
-            // title: "Mark as Complete",
+            title: null,
             text: "Complete",
             color: "green",
             callback: function (t, opts) {
