4ecfdb5f14a25d42e294b335a06a281f83eb5ee1
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-14 12:08:49 -0500

    Update connector.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,10 +1,23 @@
+// const getModifyPermission = function (t) {
+
+//   const t = window.TrelloPowerUp.iframe();
+
+//   // API_KEY, API_RETURN_URL = custom env variable in netlify
+//   const authURL =
+//     "https://trello.com/1/authorize?expiration=never" +
+//     "&scope=read&key=%%API_KEY%%&callback_method=fragment" +
+//     "&return_url=%%API_RETURN_URL%%";
+
+
+// }
+
 const getCompleteDetailBadge = function(t) {
   return t
       // duecomplete = Archived, closed = Mark complete
       .card("id","closed","labels","dueComplete")
       .then(function (card) {
 
-        console.log(card)
+        console.log(card)   //temp
 
         if (!card.closed){
             return [{
@@ -13,16 +26,31 @@ const getCompleteDetailBadge = function(t) {
                 text: "Complete",
                 color: "green",
                 callback: function (t) {
+
+                    const t = window.TrelloPowerUp.iframe();
+
+                    // API_KEY, API_RETURN_URL = custom env variable in netlify
+                    const authURL =
+                      "https://trello.com/1/authorize?expiration=never" +
+                      "&scope=read&key=%%API_KEY%%&callback_method=fragment" +
+                      "&return_url=%%API_RETURN_URL%%";
+
                     // function to run on click
-                    return t.authorize().then(function(){
-                      // put the card in a complete state (e.g. mark complete, change label, archive)
-                      return t.set('card', 'shared', 'closed', true).then(function() {
-                        console.log("Card marked as complete!");
-                      }).catch(function(error) {
-                        console.error("Permission error: ", error);
-                        // Notify the user about the permission issue
+                    t.authorize(authURL)
+                      .then(function(token){
+                        // put the card in a complete state (e.g. mark complete, change label, archive)
+                        return t
+                          .set('card', 'shared', 'closed', true)
+                          .then(function() {
+                            console.log("Card marked as complete!");
+                          }).catch(function(error) {
+                            console.error("Permission error: ", error);
+                            // Notify the user about the permission issue
+                          });
+                      })
+                      .then(function(){
+                        return t.closePopup();
                       });
-                    });
                 },
             }];
         }
@@ -32,6 +60,9 @@ const getCompleteDetailBadge = function(t) {
 
 
 window.TrelloPowerUp.initialize({
+  // "on-enable": function (t){
+  //   return getModifyPermission(t)
+  // },
   "card-detail-badges": function (t) {
     // return an array of cards that adds Complete Badge
     return getCompleteDetailBadge(t)
