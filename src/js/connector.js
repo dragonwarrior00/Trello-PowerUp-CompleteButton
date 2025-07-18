7a6ba81f9b8ec7f16f095a97da12050d5d5bb34e
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-18 13:52:29 -0500

    Update connector.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,38 +1,23 @@
+var myPowerUp = {
+    'card-buttons': function (t, options) {
+        return [{
+            icon: 'https://example.com/icon.png',
+            text: 'Archive Card',
+            callback: function (t) {
+                return archiveCard(t);
+            }
+        }];
+    }
+};
 
-const getCompleteDetailBadge = function(t) {
-  return t
-      // duecomplete = Archived, closed = Mark complete
-      .card('id','closed','labels','dueComplete')
-      .then(function (card) {
-
-        console.log(card)   //temp
-
-        if (!card.closed){
-            return [{
-                // create detail badge itself
-                title: 'Mark as Complete',
-                text: 'Complete',
-                color: 'green',
-                callback: function (t) {
-
-                    t.get('card','shared').then(function (cardState){
-                        return t.set('card', 'shared', 'archived', true).then(function () {
-                            return t.closePopup();
-                        });
-                    });
-                }
-            }];
-        }
-        // return [];
+function archiveCard(t) {
+    return t.get('card', 'id').then(function (cardId) {
+        return t.set('card', 'archived', true).then(function () {
+            return t.closePopup();
+        });
     }).catch(function (error) {
         console.error('Error archiving card:', error);
     });
 }
 
-
-window.TrelloPowerUp.initialize({
-  'card-detail-badges': function (t) {
-    // return an array of cards that adds Complete Badge
-    return getCompleteDetailBadge(t)
-  },
-});
+TrelloPowerUp.initialize(myPowerUp);
