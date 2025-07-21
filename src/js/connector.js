5e41a1bded8255c8b96716af6b62420f10147a1c
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-21 13:06:59 -0500

    Update connector.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -16,11 +16,11 @@ const getCompleteDetailBadge = function(t) {
                 callback: function (t) {
                     if (t.memberCanWriteToModel('card')){
                         // return t.getAll().then(function (t){
-                        t.get('card','shared').then(function (t){
-                            return t.set('card', 'archived', true).then(function () {
-                                return t.closePopup();
-                            });
-                            // console.log(t)
+                        return t.get('card','shared').then(function (t){
+                            // return t.set('card', 'archived', true).then(function () {
+                            //     return t.closePopup();
+                            // });
+                            console.log(t)
                         }).catch(function (error) {
                             console.error('Error archiving card:', error);
                         });
