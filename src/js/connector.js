3ec3bd28853294aee11e16f61bf8ddc3c8e831a4
DragonWarrior00 <lamberthjosh@gmail.com>
2025-07-21 20:31:31 -0500

    Update connector.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -16,7 +16,7 @@ const getCompleteDetailBadge = function(t) {
                 callback: function (t) {
                     if (t.memberCanWriteToModel('card')){
                         t.get('board', 'shared').then(function (data) {
-                            console.log(JSON.stringify(t, null, 2));
+                            console.log(data);
                             console.log("Data is present...");
                             data.set('card', 'archived', true).then(function () {
                                 data.closePopup();
